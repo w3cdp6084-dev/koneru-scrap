aa840f5f4e1633f848194914872f9d007f049d33
w3cdp6084-dev <yusukew3@gmail.com>
2022-11-11 08:20:23 +0900

    404 add

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+export default function NotFound(){
+    return(
+        <div>
+            <main>
+                <h1>404</h1>
+                <h2>ページがありません</h2>
+            </main>
+        </div>
+    )
+}
