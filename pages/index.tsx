b0afa6b1efc9e08457e9bfe02d31a027a60f9d1b
w3cdp6084-dev <yusukew3@gmail.com>
2022-11-16 13:08:27 +0900

    Blakepoint test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,44 @@
 import { Input } from "@chakra-ui/react"
+import {
+  Box,
+  Flex,
+  Text,
+  IconButton,
+  Button,
+  Stack,
+  Collapse,
+  Icon,
+  Link,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  useColorModeValue,
+  useBreakpointValue,
+  useDisclosure,
+} from '@chakra-ui/react';
 
 export default function Home() {
+
   return (
-    <Input
-      w="30%"
-      m="5"
-      type="text"
-      placeholder="Input Field"
-      color="blue.800"
-      bg="blue.50"
-      rounded="2xl"
-      border="0"
-      _focus={{
-        bg: "blue.200",
-        outline: "none",
-      }}
-    />
+    <Box w={{ base: "1280px", md: "800px", lg: "500px" }}>
+        <Box bg="red.200" w={{ base: "300px", md: "400px", lg: "500px" }}>
+          This is a box 
+        </Box>
+
+      <Input
+        w="30%"
+        m="5"
+        type="text"
+        placeholder="Input Field"
+        color="blue.800"
+        bg="blue.50"
+        rounded="2xl"
+        border="0"
+        _focus={{
+          bg: "blue.200",
+          outline: "none",
+        }}
+      />
+    </Box>
   )
 }
