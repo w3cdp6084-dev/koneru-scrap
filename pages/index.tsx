826f3fc3c9233dc9fb24e748c4ce5e16d5080476
w3cdp6084-dev <yusukew3@gmail.com>
2022-11-21 07:12:48 +0900

    Card test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,3 @@
-
-import { Input } from "@chakra-ui/react"
 import {
   Box,
   Flex,
@@ -18,13 +16,16 @@ import {
   useDisclosure,
 } from '@chakra-ui/react';
 
+import Card from '../src/components/Card';
+
 export default function Home() {
 
   return (
     <Box className="wrap" w={{ base: "414px", md: "834px", lg: "100%" }} display='flex' justifyContent='center' alignItems='center'>
-        <Box className="content" w={{ base: "414px", md: "834px", lg: "100%" }} display='flex' justifyContent='center' alignItems='center' bg='gray.100'>
-          This is a box 
-        </Box>
+      <Box className="content" w={{ base: "414px", md: "834px", lg: "100%" }} display='flex' justifyContent='center' alignItems='center' bg='gray.100'>
+        This is a box 
+      </Box>
+      <Card />
     </Box>
   )
 }
