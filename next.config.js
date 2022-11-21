ee78e009d0e40a245de27c904b2032c71ff8437e
w3cdp6084-dev <yusukew3@gmail.com>
2022-11-21 09:08:19 +0900

    card layout progress

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
   swcMinify: true,
 }
 
-// next.config.js
 module.exports = {
   images: {
     remotePatterns: [
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,7 +5,6 @@ import {
   Heading,
   Text,
   Stack,
-  Avatar,
   useColorModeValue,
 } from '@chakra-ui/react';
 
