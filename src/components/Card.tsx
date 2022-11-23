b6c66d9f494deac60da7d1389f1bf1961782c55c
w3cdp6084-dev <yusukew3@gmail.com>
2022-11-23 12:17:31 +0900

    card layout

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -42,24 +42,20 @@ export default function Card() {
             fontWeight={800}
             fontSize={'sm'}
             letterSpacing={1.1}>
-            Blog
+            TAG
           </Text>
           <Heading
             color={useColorModeValue('gray.700', 'white')}
             fontSize={'2xl'}
             fontFamily={'body'}>
-            Boost your conversion rate
+            Title
           </Heading>
           <Text color={'gray.500'}>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum.
+            Content
           </Text>
         </Stack>
         <Stack mt={6} direction={'row'} spacing={4} align={'center'}>
           <Stack direction={'column'} spacing={0} fontSize={'sm'}>
-            <Text fontWeight={600}>Achim Rolle</Text>
             <Text color={'gray.500'}>Feb 08, 2021 · 6min read</Text>
           </Stack>
         </Stack>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,6 @@ export default function Footer() {
   return (
     <div>
       <Box bg="#f1f5f9" p="10">
-      
       <Text fontSize="sm" align="center">© KONEKONE. All rights reserved.</Text>
       </Box>
     </div>
