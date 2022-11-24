14d6e28f2f6d5e5f342035536eb5dea6b86a0ef3
w3cdp6084-dev <yusukew3@gmail.com>
2022-11-24 22:09:53 +0900

    progress

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,19 @@
 import {
     Box,
-    Text
+    Text,
+    useColorModeValue,
+    
   } from '@chakra-ui/react';
 
 export default function Footer() {
   return (
     <div>
-      <Box bg="#f1f5f9" p="10">
+      <Box 
+        bg="#f1f5f9"
+        p="10" 
+        bg={useColorModeValue('#f1f5f9', 'gray.800')}
+        color={useColorModeValue('gray.600', 'white')}
+        >
       <Text fontSize="sm" align="center">© KONEKONE. All rights reserved.</Text>
       </Box>
     </div>
