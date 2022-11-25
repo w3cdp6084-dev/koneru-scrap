89ed0425800d0c41c5fa501ae9116af3932f19f9
w3cdp6084-dev <yusukew3@gmail.com>
2022-11-25 09:41:11 +0900

    head title追加/言語設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { extendTheme } from '@chakra-ui/react'
 import Header from '../src/components/Header'
 import Footer from '../src/components/Footer'
 import "../styles/layout.scss";
+import Head from "next/head";
 const colors = {
   brand: {
     900: '#1a365d',
@@ -16,6 +17,9 @@ const theme = extendTheme({ colors })
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <title>タイトル</title>
+      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
