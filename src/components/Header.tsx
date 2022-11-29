e8bc2aa69a05653a93f555f3bc280a1d2ba74269
w3cdp6084-dev <yusukew3@gmail.com>
2022-11-29 20:18:47 +0900

    logo delete

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -53,14 +53,8 @@ export default function WithSubnavigation() {
           />
         </Flex>
         <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
-          <Text
-            textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
-            fontFamily={'heading'}
-            color={useColorModeValue('gray.800', 'white')}>
-            Logo
-          </Text>
 
-          <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
+          <Flex display={{ base: 'none', md: 'flex' }}>
             <DesktopNav />
           </Flex>
         </Flex>
